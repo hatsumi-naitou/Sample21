76864cd43c03e1a69897adb30dce3f8241901a6e
h.n <hatsumi.naitou@gmail.com>
2012-09-06 20:50:39 +0900

    f0906

---
+++ src/SampleClass.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+
+public class SampleClass {
+
+	public static void main(String[] args){
+		
+		HashMap map = new HashMap();     //辞書。オブジェクト型なので、何でも入る。どんなときでも使える。
+		
+		map.put("hoge", new Integer(1));   //"hoge"は、newされたString型のインスタンス
+		
+		
+		String foo = new String("test");  //
+		Boolean boo = new Boolean(false);  //
+		
+		map.put(boo, foo);
+		
+		
+		SampleClass sc = new SampleClass();
+		
+		map.put(map, sc);   //自分自身にmapを入れる。mapのアドレスを渡す。
+				
+		//オブジェクト型なので何でも入る。が、唯一、入らないものがある。それは何か。
+		int i = 1;
+		boolean t = true;  //intやbooleanなどはオブジェクト型じゃない。プリミティブ型。なので、１．４までは入らなかった。
+		
+		map.put(1, true);  //1.5は勝手に求めるようになった。１→new　Integer が自動的に渡される。　new boolean(false)が自動的に。
+		
+	}      //これがauto　boxingの機能！
+	
+}
